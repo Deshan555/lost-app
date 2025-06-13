070e5a8fed0d26f121598de81648c49d49c3a0fa
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2025-06-13 08:59:16 +0530

    feat: add MapComponent for displaying Google Maps with markers

---
+++ src/pages/components/MapComponent.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useRef } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
+
+const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+const MapComponent = ({ lat, lng }) => {
+    const mapRef = useRef(null);
+    const mapInstance = useRef(null);
+    const markerInstance = useRef(null);
+
+    useEffect(() => {
+        const loader = new Loader({
+            apiKey: apiKey,
+            version: "weekly",
+            libraries: ["maps", "marker"],
+        });
+
+        loader.load().then((google) => {
+            const position = { lat: parseFloat(lat), lng: parseFloat(lng) };
+
+            mapInstance.current = new google.maps.Map(mapRef.current, {
+                zoom: 10,
+                center: position,
+                zoomControl: false,
+                mapTypeControl: false,
+                scaleControl: false,
+                streetViewControl: false,
+                rotateControl: false,
+                fullscreenControl: false,
+            });
+            markerInstance.current = new google.maps.Marker({
+                position: position,
+                map: mapInstance.current,
+            });
+            const infoWindow = new google.maps.InfoWindow();
+
+            markerInstance.current.addListener("click", () => {
+                const pos = markerInstance.current.getPosition();
+                infoWindow.setContent(`<b>Location:</b> ${pos.lat()}, ${pos.lng()}`);
+                infoWindow.open(mapInstance.current, markerInstance.current);
+            });
+        });
+    }, []);
+useEffect(() => {
+    if (mapInstance.current && markerInstance.current) {
+        const position = { lat: parseFloat(lat), lng: parseFloat(lng) };
+        mapInstance.current.setCenter(position);
+        markerInstance.current.setPosition(position);
+    }
+}, [lat, lng]);
+
+        return <div ref={mapRef} style={{ height: "300px", width: "100%" }} />;
+};
+
+export default MapComponent;
